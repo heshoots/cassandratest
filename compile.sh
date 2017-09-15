a67eb290b07fd2d6fb39ac96dc4e87cc2d1414f6
heshoots <heshoots9999@gmail.com>
2017-09-15 15:18:18 +0100

    Use config file for running benchmark

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-docker run -it --rm  -v /root/.m2:/root/.m2  -v $(pwd):/opt/maven -w /opt/maven maven:3.2-jdk-7 mvn clean package
+docker run -it --rm  -v /root/.m2:/root/.m2  -v $(pwd):/opt/maven -w /opt/maven maven:3.2-jdk-7 mvn package
--- src/main/java/hello/GettingStarted.java
+++ src/main/java/hello/GettingStarted.java
@@ -1,7 +1,14 @@
 import com.datastax.driver.core.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Collections;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import java.util.List;
 
 public class GettingStarted {
@@ -9,17 +16,31 @@ public class GettingStarted {
     Cluster cluster;
     Session session;
 
-    cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-    session = cluster.connect("dev");
-    session.execute("DROP TABLE if exists measures");
+    cluster = Cluster.builder().addContactPoint("cassandra").build();
+    session = cluster.connect();
 
-    int numberofrows = 1000;
-    long start = System.nanoTime();
-    new DevicePerRow(session, new ValueGenerator(numberofrows), true);
-    long end = System.nanoTime();
-    long timetaken = (end-start)/1000000000;
-    System.out.println(timetaken + " Seconds taken");
-    System.out.println(numberofrows/timetaken + " Updates per second");
-    cluster.close();
+    session.execute("DROP KEYSPACE IF EXISTS dev");
+
+    try {
+      JSONParser parser = new JSONParser();
+      JSONObject config = (JSONObject) parser.parse(new FileReader("./config.json"));
+      session.execute((String) config.get("replication"));
+      session.execute("USE dev");
+
+      int numberofrows = Integer.parseInt((String) config.get("rows"));
+      long start = System.nanoTime();
+      new DevicePerRow(session, new ValueGenerator(numberofrows), true);
+      long end = System.nanoTime();
+      long timetaken = (end-start)/1000000000;
+      System.out.println(timetaken + " Seconds taken");
+      System.out.println(numberofrows/timetaken + " Updates per second");
+      cluster.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
   }
 }
